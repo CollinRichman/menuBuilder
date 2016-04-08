3b9bb9bc90763fed2715696a3f04d9c1b0f89fed
Collin Richman <richco01@luther.edu>
2016-04-07 21:34:24 -0500

    Front end CRUD functionality on Item list
    Adding edit, add, and delete functionality to the item list. Still no
    functionality for having multiple menus., And no connection to node js
    yet.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,49 +1,91 @@
 var myApp = angular.module("myApp", []);
 myApp.controller('AppCtrl', function($scope, $http) {
 
-  var refresh = function() {
-    $http.get('/contactlist').success(function(response) {
-      $scope.contactlist = response;
-      $scope.contact = '';
-    });
-
+  $scope.model = {
+    items: [{
+          name: "Pizza",
+          price: 10
+      }, {
+          name: "Chicken",
+          price: 15
+      }, {
+          name: "Salad",
+          price: 32
+      }],
+      selectedItem: {},
+      selectedIndex: -1
   };
 
-  refresh();
-  //$scope.contactlist = contactlist;
-
-  $scope.addContact = function() {
-    $http.post('/contactlist', $scope.contact).success(
-      function(response) {
-        console.log(response);
-        refresh();
-      }
-    );
-
+  // gets the template to ng-include for a table row / item
+  $scope.getTemplate = function (idx) {
+      if (idx === $scope.model.selectedIndex) return 'edit';
+      else return 'display';
   };
 
-
-  $scope.remove = function(contact) {
-    console.log(contact);
-		console.log("ANGULAR message - remove");
-    //$http.delete('/contactlist/' + id);
-
-    $http.post('/contactlist/delete', contact).success(
-      function(response) {
-        refresh();
-      }
-    );
-
+  $scope.editItem = function (item, idx) {
+      $scope.model.selectedItem = angular.copy(item);
+      $scope.model.selectedIndex = idx;
   };
 
-  $scope.edit = function(contact) {
-    $scope.contact = contact;
+  $scope.saveItem = function (idx) {
+      $scope.model.items[idx] = angular.copy($scope.model.selectedItem);
+      $scope.reset();
   };
 
-  $scope.update = function() {
-    console.log($scope.contact);
-    $http.put('/contactlist/' + $scope.contact._id, $scope.contact);
+  $scope.reset = function () {
+      $scope.model.selectedItem = {};
+      $scope.model.selectedIndex = -1;
   };
+
+  $scope.addItem = function(){
+      var newItem = {name: "",price: ""}
+      $scope.model.items.push(newItem);
+      $scope.editItem(newItem,$scope.model.items.length-1);
+  }
+
+  $scope.deleteItem = function(idx){
+      var removedItem = $scope.model.items.splice(idx,1);
+      $scope.reset();
+  }
+
+  // var refresh = function() {
+  //   $http.get('/contactlist').success(function(response) {
+  //     $scope.contactlist = response;
+  //     $scope.contact = '';
+  //   });
+  // };
+
+  //refresh();
+
+  // $scope.addContact = function() {
+  //   $http.post('/contactlist', $scope.contact).success(
+  //     function(response) {
+  //       console.log(response);
+  //       refresh();
+  //     }
+  //   );
+  // };
+
+
+  // $scope.remove = function(contact) {
+  //   console.log(contact);
+	// 	console.log("ANGULAR message - remove");
+  //   //$http.delete('/contactlist/' + id);
+  //   $http.post('/contactlist/delete', contact).success(
+  //     function(response) {
+  //       refresh();
+  //     }
+  //   );
+  // };
+
+  // $scope.edit = function(contact) {
+  //   $scope.contact = contact;
+  // };
+
+  // $scope.update = function() {
+  //   console.log($scope.contact);
+  //   $http.put('/contactlist/' + $scope.contact._id, $scope.contact);
+  // };
 
 
 });
