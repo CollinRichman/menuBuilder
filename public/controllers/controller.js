eeb54893d4bb278f509c752ab04149fe57734e72
Collin Richman <richco01@luther.edu>
2016-04-20 21:14:32 -0500

    Comments, logging, and formatting

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,93 +1,113 @@
+//Angular application module
 var myApp = angular.module("myApp", []);
+
+//Add an Angular controller
 myApp.controller('AppCtrl', function($scope, $http) {
 
-  var init = function(){
-      fetchMenuList();
-      if ($scope.model === undefined){
-          $scope.model = {menus: [],selectedMenu: {}};
-      }
-  }
-
-  var fetchMenuList = function() {
-    $http.get('/getMenuList').success(function(response) {
-      $scope.model.menus = response;
-      $scope.model.selectedMenu = {};
-    });
-  };
-
-  $scope.onMenuClicked = function(menu){
-    $scope.model.selectedMenu.isSelected = false;
-    menu.isSelected = true;
-    $scope.model.selectedMenu = menu;
-  }
-
-  $scope.isAnyMenuSelected = function(){
-      return !angular.equals($scope.model.selectedMenu,{});
-  }
-
-  $scope.getSelectedMenuName = function(){
-    if(!$scope.isAnyMenuSelected()){
-      return "No Menu Selected";
+    //called when page loads
+    var init = function() {
+        //Attempt to populate $scope.model from Node server
+        fetchMenuList();
+        //If $scope.model is still undefined, initiate it
+        if ($scope.model === undefined) {
+            $scope.model = {
+                menus: [],
+                selectedMenu: {}
+            };
+        }
     }
-    if ($scope.model.selectedMenu.name === undefined || $scope.model.selectedMenu.name === ""){
-      return "Enter Menu Name";
+
+    //Fetch menuList from Node server
+    var fetchMenuList = function() {
+        //Send a GET request at /getMenuList
+        $http.get('/getMenuList').success(function(response) {
+            $scope.model.menus = response;
+            $scope.model.selectedMenu = {};
+        });
+    };
+
+    //Adding $scope adds this function to the controller's scope, allowing it to be called from Angular directives
+    $scope.onMenuClicked = function(menu) {
+        $scope.model.selectedMenu.isSelected = false;
+        menu.isSelected = true;
+        $scope.model.selectedMenu = menu;
     }
-    return $scope.model.selectedMenu.name;
-  }
 
-  $scope.addMenu = function(){
-      var newMenu = {name: "",isSelected: false, items: []}
-      $http.post('/addNewMenu', newMenu).success(
-        function(response) {
-          newMenu = response;
-          $scope.model.menus.push(newMenu);
-          $scope.onMenuClicked(newMenu);
+    $scope.isAnyMenuSelected = function() {
+        return !angular.equals($scope.model.selectedMenu, {});
+    }
+
+    $scope.getSelectedMenuName = function() {
+        if (!$scope.isAnyMenuSelected()) {
+            return "No Menu Selected";
         }
-      );
-  }
-
-  $scope.deleteMenu = function(idx){
-      var removedMenu = $scope.model.menus[idx];
-      $http.post('/deleteMenu', removedMenu).success(
-        function(response) {
-          if(angular.equals($scope.model.menus[idx],$scope.model.selectedMenu)){
-              $scope.model.selectedMenu = {};
-          }
-          $scope.model.menus.splice(idx,1);
+        if ($scope.model.selectedMenu.name === undefined || $scope.model.selectedMenu.name === "") {
+            return "Enter Menu Name";
         }
-      );
-  }
+        return $scope.model.selectedMenu.name;
+    }
 
-  $scope.saveMenu = function(menu){
-      $http.put('/updateMenu/', menu);
-  }
-
-  $scope.addItem = function(){
-      var newItem = {name: "", price: "", parent: $scope.model.selectedMenu._id}
-      $http.post('/addNewItem', newItem).success(
-        function(response) {
-          newItem = response;
-          $scope.model.selectedMenu.items.push(newItem);
+    $scope.addMenu = function() {
+        var newMenu = {
+            name: "",
+            isSelected: false,
+            items: []
         }
-      );
+        $http.post('/addNewMenu', newMenu).success(
+            function(response) {
+                newMenu = response;
+                $scope.model.menus.push(newMenu);
+                $scope.onMenuClicked(newMenu);
+            }
+        );
+    }
 
-  }
+    $scope.deleteMenu = function(idx) {
+        var removedMenu = $scope.model.menus[idx];
+        $http.post('/deleteMenu', removedMenu).success(
+            function(response) {
+                if (angular.equals($scope.model.menus[idx], $scope.model.selectedMenu)) {
+                    $scope.model.selectedMenu = {};
+                }
+                $scope.model.menus.splice(idx, 1);
+            }
+        );
+    }
 
-  $scope.deleteItem = function(idx){
-      var removedItem = $scope.model.selectedMenu.items[idx];
+    $scope.saveMenu = function(menu) {
+        $http.put('/updateMenu/', menu);
+    }
 
-      $http.post('/deleteItem', removedItem).success(
-        function(response) {
-          $scope.model.selectedMenu.items.splice(idx,1);
+    $scope.addItem = function() {
+        var newItem = {
+            name: "",
+            price: "",
+            parent: $scope.model.selectedMenu._id
         }
-      );
+        $http.post('/addNewItem', newItem).success(
+            function(response) {
+                newItem = response;
+                $scope.model.selectedMenu.items.push(newItem);
+            }
+        );
 
-  }
+    }
 
-  $scope.saveItem = function(item){
-      $http.put('/updateItem/', item);
-  }
+    $scope.deleteItem = function(idx) {
+        var removedItem = $scope.model.selectedMenu.items[idx];
 
-  init();
+        $http.post('/deleteItem', removedItem).success(
+            function(response) {
+                $scope.model.selectedMenu.items.splice(idx, 1);
+            }
+        );
+
+    }
+
+    $scope.saveItem = function(item) {
+        $http.put('/updateItem/', item);
+    }
+
+    init();
 
 });
--- server.js
+++ server.js
@@ -1,44 +1,49 @@
+//npm packages
 var express = require('express');
 var mongojs = require('mongojs');
 var bodyParser = require('body-parser');
 var ObjectID = require('mongodb').ObjectID;
 
 var app = express();
+
+//set up express middleware
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
+
+//Tell server to listen on port 3000
 app.listen(3000);
 console.log('Server running on port 30000');
 
+//get DB connection from database: 'menuList', collection: 'menuList'
+//If this database or collection does not exist, mongojs will create it
 var db = mongojs('menuList', ['menuList']);
+
 db.on('error', function(err) {
     console.log('database error', err)
 })
+
 db.on('connect', function() {
     console.log('database connected')
 })
 
-/*
-Fetch Menu List
-*/
+//Listen for GET requests on /getMenuList
 app.get('/getMenuList', function(req, res) {
+    console.log('Received GET request for /getMenuList')
     db.menuList.find(function(err, docs) {
+        //send back JSON response
         res.json(docs);
     });
 });
 
-/*
-Add a New Menu
-*/
 app.post('/addNewMenu', function(req, res) {
+    console.log('Received POST request for /addNewMenu')
     db.menuList.insert(req.body, function(err, doc) {
         res.json(doc);
     });
 });
 
-/*
-Delete a Menu
-*/
 app.post('/deleteMenu', function(req, res) {
+    console.log('Received POST request for /deleteMenu')
     var menu = req.body;
     db.menuList.remove({
             _id: mongojs.ObjectId(menu._id)
@@ -49,11 +54,8 @@ app.post('/deleteMenu', function(req, res) {
     );
 });
 
-/*
-Update a Menu (name change)
-*/
 app.put('/updateMenu', function(req, res) {
-    console.log("server.updateMenu");
+    console.log('Received PUT request for /updateMenu')
     var menu = req.body;
     var id = menu._id;
     db.menuList.findAndModify({
@@ -72,11 +74,9 @@ app.put('/updateMenu', function(req, res) {
         });
 });
 
-/*
-Add an item to a menu
-*/
 app.post('/addNewItem', function(req, res) {
-    //generate a unique id for the item
+    console.log('Received POST request for /addNewItem')
+        //generate a unique id for the new item
     var objectId = new ObjectID();
     var item = req.body;
     item._id = objectId;
@@ -92,19 +92,17 @@ app.post('/addNewItem', function(req, res) {
             new: true
         },
         function(err, doc) {
-            res.json(doc.items[doc.items.length-1]);
+            res.json(doc.items[doc.items.length - 1]);
         });
 });
 
-/*
-Update an item
-*/
 app.put('/updateItem', function(req, res) {
+    console.log('Received PUT request for /updateItem')
     var item = req.body;
     db.menuList.findAndModify({
             query: {
                 "_id": mongojs.ObjectId(item.parent),
-								"items._id": mongojs.ObjectId(item._id)
+                "items._id": mongojs.ObjectId(item._id)
             },
             update: {
                 $set: {
@@ -119,27 +117,23 @@ app.put('/updateItem', function(req, res) {
         });
 });
 
-/*
-Delete an item from a menu
-*/
 app.post('/deleteItem', function(req, res) {
+    console.log('Received POST request for /deleteItem')
     var item = req.body;
-		console.log("deleting:");
-		console.log(item);
     db.menuList.findAndModify({
             query: {
                 "_id": mongojs.ObjectId(item.parent)
             },
             update: {
                 $pull: {
-                    items: { _id: mongojs.ObjectId(item._id)}
+                    items: {
+                        _id: mongojs.ObjectId(item._id)
+                    }
                 }
             },
             new: true
         },
         function(err, doc) {
-						console.log("deleted:");
-						console.log(doc);``
             res.json(doc);
         });
 });
